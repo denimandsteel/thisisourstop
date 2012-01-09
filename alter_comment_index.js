841e746512d88eb424a13a742bac6784e18e5aa7
Tylor Sherman <tylorsherman@gmail.com>
2012-01-09 13:44:37 -0800

    Check table

--- alter_comment_index.js
+++ alter_comment_index.js
@@ -6,7 +6,15 @@ var count = 0;
 client = new pg.Client(connectionString);
 client.connect();
 
-var query = client.query("ALTER TABLE comments ALTER COLUMN id SET DEFAULT nextval('comment_id')");
+var query = client.query("select * from INFORMATION_SCHEMA.COLUMNS where table_name = 'comments';");
+query.on('row', function(row) {
+  console.log(row);
+});
 query.on('end', function() {
   client.end();
 });
+
+/*var query = client.query("ALTER TABLE comments ALTER COLUMN id SET DEFAULT nextval('comment_id')");
+query.on('end', function() {
+  client.end();
+});*/
