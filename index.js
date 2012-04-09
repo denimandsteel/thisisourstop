ab3955ce70bec932eaad1b84168d39252099dd8a
Tylor Sherman <tylorsherman@gmail.com>
2012-04-09 16:18:44 -0700

    A long time ago… nicer timestamps for way back.

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ function prettyDate(time){
     day_diff = Math.floor(diff / 86400);
 
   if ( isNaN(day_diff) || day_diff < 0 || day_diff >= 31 )
-    return;
+    return 'A long time ago';
 
   return day_diff == 0 && (
       diff < 60 && "Just now" ||
