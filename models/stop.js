2f0b96b91513724a8bafd5519fb49914d6dc1e55
Tylor Sherman <tylorsherman@gmail.com>
2012-04-09 15:27:01 -0700

    Broken date handling and separate admin stop get.

--- models/stop.js
+++ models/stop.js
@@ -11,6 +11,28 @@ var Stop = exports = module.exports = function Stop(id) {
   this.id = id;
 };
 
+exports.getShort = function(id, fn){
+  client.query('SELECT * FROM stops WHERE stop_code = $1', [id], function(err, result) {
+    if (result.rows.length > 0) {
+      var ret = new Stop(id);
+      var data = result.rows[0];
+      for (var key in data) {
+        if (undefined != data[key]) {
+          ret[key] = data[key];
+        }
+        // Ugly... dirty data...
+        if (key == 'stop_desc') {
+          ret[key] = formatTitles(data[key]).replace('@', '<span class="minor">at</span>');
+        }
+      }
+      fn(null, ret);
+    }
+    else {
+      fn('No such stop.');
+    }
+  });
+};
+
 exports.get = function(id, fn){
   client.query('SELECT * FROM stops WHERE stop_code = $1', [id], function(err, result) {
     if (result.rows.length > 0) {
@@ -35,8 +57,8 @@ exports.get = function(id, fn){
           ret.trip[i] = {
             route_number: headsign[1],
             route_name: formatTitles(headsign[2]),
-            arrival_time: arrival.getHours() % 12 + ':' + (arrival.getMinutes() < 10 ? '0' : '') + arrival.getMinutes(),
-            arrive_soon: ((arrival.getMinutes() - (new Date()).getMinutes()) < 5 ? ' soon' : ''),
+            arrival_time: (arrival.getHours() % 12  === 0 ? 12 : arrival.getHours() % 12) + ':' + (arrival.getMinutes() < 10 ? '0' : '') + arrival.getMinutes(),
+            arrive_soon: (arrival.getTime() - (new Date()).getTime()) / (1000 * 60) < 5 ? ' soon' : '',
             night: headsign[1].indexOf("N") === 0 ? ' night' : ''
           }
           //ret.trip[i].route_short_name = trip.rows[i].route_short_name.replace(/^[0]+/g,''); // Remove leading zeros.
@@ -48,84 +70,82 @@ exports.get = function(id, fn){
       fn('No such stop.');
     }
   });
-
-  // We have some seriously ugly data from Translink, let's make it nicer.
-  // Extended from: http://ejohn.org/blog/title-capitalization-in-javascript/
-  var formatTitles = function(str) {
-    var small = "(a|an|and|as|at|but|by|en|for|if|in|of|on|or|the|to|v[.]?|via|vs[.]?)";
-    var big = "(UBC|VCC|BCIT|SFU)";
-    var correct = {
-      'Bdry': 'Boundary',
-      'Bway': 'Broadway',
-      'Gran': 'Granville',
-      'Av': 'Ave',
-      'Cntrl': 'Central',
-      'Exch': 'Exchange',
-      'Ex': 'Exchange',
-      'Cap': 'Capilano',
-      'U': 'University',
-      'Van': 'Vancouver',
-      'Coq': 'Coquitlam',
-      'Mdws': 'Meadows',
-      'Rvr': 'River',
-      'Stat': 'Stn',
-    }
-    // Get keys for use in regex.
-    var correct_keys = [];
-    for (var key in correct) {
-      if(Object.prototype.hasOwnProperty.call(correct, key)) {
-        correct_keys.push(key);
-      }
-    }
-
-    var punct = "([!\"#$%&'()*+,./:;<=>?@[\\\\\\]^_`{|}~-]*)";
-
-    this.titleCaps = function(title){
-      var parts = [],
-          split = /[:.;?!] |(?: |^)["Ò]/g,
-          index = 0;
-
-      while (true) {
-        var m = split.exec(title);
-
-        parts.push(title
-          .toLowerCase()
-          .substring(index, m ? m.index : title.length)
-          .replace(/\b([A-Za-z][a-z.'Õ]*)\b/g, function(all){
-            return /[A-Za-z]\.[A-Za-z]/.test(all) ? all : upper(all);
-          })
-          .replace(RegExp("\\b" + small + "\\b", "ig"), function(word) {
-            return word.toLowerCase();
-          })
-          .replace(RegExp("^" + punct + small + "\\b", "ig"), function(all, punct, word){
-            return punct + upper(word);
-          })
-          .replace(RegExp("\\b" + small + punct + "$", "ig"), upper)
-        );
-
-        index = split.lastIndex;
-
-        if (m) {
-          parts.push(m[0]);
-        }
-        else {
-          break;
-        }
-      }
-
-      return parts.join("").replace(/ V(s?)\. /ig, " v$1. ")
-        .replace(/(['Õ])S\b/ig, "$1s")
-        .replace(RegExp("\\b(" + correct_keys.join("|") + ")\\b", "ig"), function(all) {
-          return correct[all];
-        })
-        .replace(RegExp("\\b" + big + "\\b", "ig"), function(all){
-          return all.toUpperCase();
-        });
-    };
-
-    function upper(word){
-      return word.substr(0,1).toUpperCase() + word.substr(1);
-    }
-    return this.titleCaps(str);
-  }
 };
+
+// We have some seriously ugly data from Translink, let's make it nicer.
+// Extended from: http://ejohn.org/blog/title-capitalization-in-javascript/
+var formatTitles = function(str) {
+  var small = "(a|an|and|as|at|but|by|en|for|if|in|of|on|or|the|to|v[.]?|via|vs[.]?)";
+  var big = "(UBC|VCC|BCIT|SFU)";
+  var correct = {
+    'Bdry': 'Boundary',
+    'Bway': 'Broadway',
+    'Gran': 'Granville',
+    'Av': 'Ave',
+    'Cntrl': 'Central',
+    'Exch': 'Exchange',
+    'Ex': 'Exchange',
+    'Cap': 'Capilano',
+    'U': 'University',
+    'Van': 'Vancouver',
+    'Coq': 'Coquitlam',
+    'Mdws': 'Meadows',
+    'Rvr': 'River',
+    'Stat': 'Stn',
+  }
+  // Get keys for use in regex.
+  var correct_keys = [];
+  for (var key in correct) {
+    if(Object.prototype.hasOwnProperty.call(correct, key)) {
+      correct_keys.push(key);
+    }
+  }
+
+  var punct = "([!\"#$%&'()*+,./:;<=>?@[\\\\\\]^_`{|}~-]*)";
+
+  this.titleCaps = function(title){
+    var parts = [],
+        split = /[:.;?!] |(?: |^)["Ò]/g,
+        index = 0;
+
+    while (true) {
+      var m = split.exec(title);
+
+      parts.push(title
+        .toLowerCase()
+        .substring(index, m ? m.index : title.length)
+        .replace(/\b([A-Za-z][a-z.'Õ]*)\b/g, function(all){
+          return /[A-Za-z]\.[A-Za-z]/.test(all) ? all : upper(all);
+        })
+        .replace(RegExp("\\b" + small + "\\b", "ig"), function(word) {
+          return word.toLowerCase();
+        })
+        .replace(RegExp("^" + punct + small + "\\b", "ig"), function(all, punct, word){
+          return punct + upper(word);
+        })
+        .replace(RegExp("\\b" + small + punct + "$", "ig"), upper)
+      );
+
+      index = split.lastIndex;
+
+      if (m) {
+        parts.push(m[0]);
+      }
+      else {
+        break;
+      }
+    }
+
+    return parts.join("").replace(/ V(s?)\. /ig, " v$1. ")
+      .replace(/(['Õ])S\b/ig, "$1s")
+      .replace(RegExp("\\b(" + correct_keys.join("|") + ")\\b", "ig"), function(all) {
+        return correct[all];
+      })
+      .replace(RegExp("\\b" + big + "\\b", "ig"), function(all){
+        return all.toUpperCase();
+      });
+  };
+
+  function upper(word){
+    return word.substr(0,1).toUpperCase() + word.substr(1);
+  }
